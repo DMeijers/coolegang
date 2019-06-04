56bcbdbedb98f0d6f8ffa4612680ef354592aed9
Piet <pj.mens121@gmail.com>
2019-06-04 12:30:23 +0200

    Homepage container

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,13 +2,15 @@
 	<head>
 		<title>Page Title</title>
 	</head>
-	<body>
+	<body style="height: 100%;">
+	<div class="uk-background-muted uk-background-cover" style="height: 100%;">
+
 		<div>
 
 		</div>
 
 		<!-- Container -->
-		<div class="uk-container uk-background-primary uk-width-1-3 uk-height-1 uk-position-center">
+		<div class="uk-container uk-background-default uk-width-1-3 uk-height-1 uk-position-center">
 			<!-- Post -->
 			<div class="uk-width-5-6"> 
 				<!-- User -->
@@ -18,8 +20,21 @@
 					</div>
 					<!-- Username -->
 					<div>
+						GoodGuyThicc
+					</div>
+				</div>
+				<!-- Content -->
+				<div>
+					<!-- Description -->
+					<div>
+						Yow dit is een coole post man
+					</div>
+					<!-- Picture/Video -->
+					<div>
+
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 	</body>
