183d60a78a946a729acae1ae44a1639539edd5db
rico <ricotbrink@live.nl>
2019-06-04 12:33:47 +0200

    Merge branch 'master' of /home/rico/Documents/Webapp/ananas with conflicts.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,15 +2,26 @@
 	<head>
 		<title>Page Title</title>
 	</head>
-	<body style="height: 100%;">
-	<div class="uk-background-muted uk-background-cover" style="height: 100%;">
-
+	<body>
 		<div>
-
+			<nav class="uk-navbar-container" uk-navbar>
+				<div class="uk-navbar-left">
+					<ul class="uk-navbar-nav">
+						<li><a href="{{ route('homepage') }}">Home</a></li>
+						<li><a href="{{ route('welcomepage') }}">Welcome</a></li>
+					</ul>
+				</div>
+				<div class="uk-navbar-right">
+					<ul class="uk-navbar-nav">
+						<li><a href="{{ route('login') }}">Login</a></li>
+						<li><a href="{{ route('register') }}">Register</a></li>
+					</ul>
+				</div>
+			</nav>
 		</div>
 
 		<!-- Container -->
-		<div class="uk-container uk-background-default uk-width-1-3 uk-height-1 uk-position-center">
+		<div class="uk-container uk-background-primary uk-width-1-3 uk-height-1 uk-position-center">
 			<!-- Post -->
 			<div class="uk-width-5-6"> 
 				<!-- User -->
@@ -20,21 +31,8 @@
 					</div>
 					<!-- Username -->
 					<div>
-						GoodGuyThicc
-					</div>
-				</div>
-				<!-- Content -->
-				<div>
-					<!-- Description -->
-					<div>
-						Yow dit is een coole post man
-					</div>
-					<!-- Picture/Video -->
-					<div>
-
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	</body>
