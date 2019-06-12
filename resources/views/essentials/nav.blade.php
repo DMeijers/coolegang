1d1749d84a4a30dcfc28b9d0c46e2d4a7e5148a7
Dieko <admeijers@hotmail.nl>
2019-06-12 22:30:14 +0200

    een net iets betere nav bar

--- resources/views/essentials/nav.blade.php
+++ resources/views/essentials/nav.blade.php
@@ -1,16 +1,26 @@
 <nav uk-navbar>
     <div class="uk-navbar-left">
 
-        <a href="#offcanvas-usage" uk-toggle><span class="pl-4" uk-icon="menu"></span></a>
+        <a href="#offcanvas-usage" uk-toggle="target: #offcanvas-nav-primary"><span class="pl-4" uk-icon="menu"></span></a>
 
-        <div id="offcanvas-usage" uk-offcanvas>
-            <div class="uk-offcanvas-bar">
+        <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true">
+            <div class="uk-offcanvas-bar uk-flex uk-flex-column">
 
-                <button class="uk-offcanvas-close" type="button" uk-close></button>
-
-                <h3>Title</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <ul class="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                    <li class="uk-active"><a href="#">Active</a></li>
+                    <li class="uk-parent">
+                        <a href="#">Parent</a>
+                        <ul class="uk-nav-sub">
+                            <li><a href="#">Sub item</a></li>
+                            <li><a href="#">Sub item</a></li>
+                        </ul>
+                    </li>
+                    <li class="uk-nav-header">Header</li>
+                    <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: table"></span> Item</a></li>
+                    <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> Item</a></li>
+                    <li class="uk-nav-divider"></li>
+                    <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: trash"></span> Item</a></li>
+                </ul>
 
             </div>
         </div>
